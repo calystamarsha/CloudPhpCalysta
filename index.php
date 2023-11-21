454d48daa869361f5996a112e8b8e966a78198f2
calystamarsha <150098518+calystamarsha@users.noreply.github.com>
2023-11-21 11:42:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Nama : Calysta Marsha. ";
-echo "Nim : 51022004.";
-echo "Jurusan : Sistem Informasi.";
+echo "Nama : Calysta Marsha.\n ";
+echo "Nim : 51022004.\n ";
+echo "Jurusan : Sistem Informasi.\n ";
